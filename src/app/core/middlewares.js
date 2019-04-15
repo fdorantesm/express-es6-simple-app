a8089bf641d06eb5d02198af1058bad92f51d2c8
Fernando D <fernando@dorant.es>
2019-04-15 04:46:12 -0500

    Replaced res.send response for res.boom

--- src/app/core/middlewares.js
+++ src/app/core/middlewares.js
@@ -18,18 +18,16 @@ export default function middlewares(app) {
   router(app);
 
   /**
-     * 404 Error Handler
-     */
+   * 404 Error Handler
+   */
   app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    res.boom.notFound();
   });
 
   /**
-     * Error Handler
-     */
-    app.use((error, req, res, next) => { // eslint-disable-line
+   * Error Handler
+   */
+  app.use((error, req, res, next) => { // eslint-disable-line
     res.status(error.status || 500).send(error);
   });
 
