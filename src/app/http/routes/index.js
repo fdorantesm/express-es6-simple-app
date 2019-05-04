62cb195b820ea8f59e3f043efbdc45d011c48785
Fernando D <fernando@dorant.es>
2019-05-03 23:28:48 -0500

    Log express registered routes
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/http/routes/index.js

--- src/app/http/routes/index.js
+++ src/app/http/routes/index.js
@@ -1,8 +1,33 @@
 import Router from 'router';
-import teams from 'routes/teams';
+import path from 'path';
+import glob from 'glob';
+import CliTable from 'cli-table';
+import trimEnd from 'lodash/trimEnd';
+
+// eslint-disable-next-line no-unused-vars
+const table = new CliTable({
+  head: ['', 'Path'],
+  colWidths: [20, 40],
+});
 
 const router = Router(); // eslint-disable-line
 
-router.use('/teams', teams);
+glob.sync(process.env.APP_PATH + '/http/routes/*.js').forEach((file) => {
+  if (file !== __filename) {
+    const routes = require(file).default;
+    const base = `/${path.basename(file).replace('.js', '')}`;
+    router.use(base, routes);
+    // eslint-disable-next-line max-len
+    routes.stack.filter((r) => r.route).map((r) => {
+      // eslint-disable-next-line max-len
+      Object.keys(r.route.methods).map((method) => {
+        // eslint-disable-next-line max-len
+        table.push([method.toUpperCase(), trimEnd(base.concat(r.route.path), '/')]);
+      });
+    });
+  }
+});
+
+console.log(table.toString());
 
 export default router;
